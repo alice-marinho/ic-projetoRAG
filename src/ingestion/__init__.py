eab70238e911968367846e3928aec830dd01695a
alice <alicemarinhogms@gmail.com>
2025-06-15 18:43:08 -0300

    refactor: melhora estrutura da classe DocsExtractor

--- src/ingestion/__init__.py
+++ src/ingestion/__init__.py
@@ -1,6 +1,6 @@
 from .cleanner import TextCleaner
-from .extractor import extract_fields
+from .extractor import DocsExtractor
 from .loader import load_documents
 from .splitter import split_documents, split_json
 
-__all__ = ["TextCleaner", "extract_fields", "load_documents", "split_documents", "split_json"]
+__all__ = ["TextCleaner", "DocsExtractor", "load_documents", "split_documents", "split_json"]

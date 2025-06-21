b7ac598ac19e4ae814afd672cc74978e2a107b4d
alice <alicemarinhogms@gmail.com>
2025-06-21 02:09:53 -0300

    refactor: melhorias na classe TextCleaner

--- src/config/config.py
+++ src/config/config.py
@@ -33,21 +33,28 @@ REGEX_FIELDS = {
     "Componente curricular": r"(?:Componente:|Componente curricular:)\s*(.*)",
     "Semestre": r"Semestre:\s*(\d+)",
     "Ano": r"Ano:\s*(\d+)",
-    "Código": r"(?:Código:|Sigla:)\s*(.*)",
+    "Código": r"(?:C[óo]digo:|Sigla:)\s*(.*)",
     "Tipo": r"Tipo:\s*(.*)",
-    "Núcleo": r"(?:Núcleo:|Nucleo:)\s*(.*)",
-    "N° de docentes": r"(?:quantidade|[Nn]\.?\°?\s*\.?|número)?\s*(?:de)?\s*docente[s]?:\s*(\d*)",
-    "Nº de aulas semanais": r"(?:quantidade de|n\.?\s*°?\s*|n\.?\s*°?\s*de|número de)?\s*aulas \s*semanais[:\s]*\s*(\d+)",
+    "Núcleo": r"(?:N[úu]cleo[:-])\s*(.*)",
+    "Número de docentes": r"(?:quantidade|[Nn]\.?\°?\s*\.?|número)?\s*(?:de)?\s*docente[s]?:\s*(\d*)",
+    "Número de aulas semanais": r"(?:quantidade de|n\.?\s*°?\s*|n\.?\s*°?\s*de|número de)?\s*aulas \s*semanais[:\s]*\s*(\d+)",
     "Total de aulas": r"Total de aulas:\s*(\d+)",
-    "C.H. Ensino": r"C\.H\. Ensino:\s*(\d+[.,]?\d*)",
-    "C.H. Presencial": r"C\.H\. Presencial:\s*(.*)",
+    "CH Ensino": r"C\.?H\.?\s+Ensino[:-]\s*(\d+[.,]?\d*)",
+    "CH Presencial": r"C\.?H\.?\s+Presencial[:-]\s*([\d,.]+)",
+    "CH a Distância": r"C\.?H\.?\s+a?\s+Dist[aâ]ncia[:-]\s*([\d,.]+)",
     "Abordagem Metodológica": r"""T\s*\(\s*([Xx])?\s*\)\s*P\s*\(\s*([Xx])?\s*\)\s*(?:T/P\s*\(\s*([Xx])?\s*\)|\(\s*([Xx])?\s*\)\s*T/P)""",
-    "Lab": r"(?:\(\s*([Xx])?\s*\)\s*SIM\s*\(\s*([Xx])?\s*\)\s*N[ÃA]O|(?:carga.*)laborat[óo]rio[:\s]*\n*\s*(\d+[.,]?\d*))", #r"(?:Uso de.*?laboratório|Carga horária.*?laboratório).*?[:\?]\s*(.*)"
+    "Lab": r"(?:\(\s*([Xx])?\s*\)\s*SIM\s*\(\s*([Xx])?\s*\)\s*N[ÃA]O|(?:carga.*)laborat[óo]rio[:\s]*\n*\s*(\d+[.,]?\d*))",
     "Grupos de Conhecimentos Essenciais": r"2\s*-\s*(?:GRUPOS\s+DE\s+)?Conhecimentos essenciais do curr[íi]culo de refer[êe]ncia\s*:?\s*(.*?)(?=\n?\s*\d+\s*-\s*)",
-    "Ementa": r"Ementa\s*[:\-]?\s*(.*?)(?=\n\d+\s*[-–—]\s*[A-Z]|(?=4\s*-\s*OBJETIVOS|$))",
-    "Objetivos": r"OBJETIVOS\s*[:\-–—]?\s*(.*?)(?=\n\d+\s*[-–—]\s*[A-Z]|(?=\n\d+\s*[-–—]\s*ÁREAS DE INTEGRAÇÃO))",
-    "Conteúdo Programático": r"5\s*[-–]\s*CONTEÚDO\s+PROGRAMÁTICO\s*[:\-]?\s*(.*?)(?=\n?6\s*[-–]\s*(?:METODOLOGIA|BIBLIOGRAFIA)|$)"
+    "Ementa": r"Ementa\s*[:\-]?\s*(.*?)(?=\n\d+\s*[-–—]\s*[A-Z]|(?=\n?\s*\d+\s*-\s*))",
+    "Objetivos": r"OBJETIVOS\s*[:\-–—]?\s*(.*?)(?=\n?\s*\d+\s*-\s*|$)",
+    "Conteúdo Programático": r"\d+\s*[-–]\s*CONTE[ÚU]DO\s+PROGRAM[ÁA]TICO\s*[:\-]?\s*(.*?)(?=\n?\s*\d+\s*-\s*|$)"
 }
+
+WORKLOAD = {
+    "CH Ensino", "CH Presencial", "CH a Distância"
+}
+
+
 
 MULTILINE_FIELDS = {
     "Grupos de Conhecimentos Essenciais",
--- src/ingestion/extractor.py
+++ src/ingestion/extractor.py
@@ -1,5 +1,5 @@
 import json
-import logging
+from utils.logger import setup_logger
 import re
 from typing import List, Dict, Any, LiteralString, Optional
 
@@ -11,13 +11,23 @@ from config.config import REGEX_FIELDS,MULTILINE_FIELDS
 class DocsExtractor:
     def __init__(
         self,
-        raw_data_file = config.RAW_DATA_FILE
+        raw_data_file = config.RAW_DATA_FILE,
     ):
         self.raw_data_file = raw_data_file
 
 
     def extract_fields(self, texto: str)-> list[dict[Any, str | LiteralString]]:
         extracted_data = []
+        logger = setup_logger(__name__)
+
+        # Ele vai procurar "Planos de ensino" somente se tiver em seguida de 1- Identificação
+
+        start_pattern = r"(?s)(Planos de ensino.*?1-\s*IDENTIFICAÇÃO.*)"
+        match_planos = re.search(start_pattern, texto, re.IGNORECASE)  # DOTALL já está no (?s)
+
+        if not match_planos:
+            logger.warning("Seção 'Planos de ensino' seguida por '1- IDENTIFICAÇÃO' não encontrada.")
+            return []  # Retorna lista vazia se não encontrar a seção
         # Divide o texto em seções
         sections = re.split(r"\b1-\s*IDENTIFICAÇÃO\b", texto)
 
@@ -45,13 +55,14 @@ class DocsExtractor:
                 extracted_data.append(dados)
         with open(self.raw_data_file, "w", encoding="utf-8") as f:
             json.dump(extracted_data, f, ensure_ascii=False, indent=4)
-        logging.debug("JSON gerado com sucesso: dados_brutos.json")
+        logger.debug("JSON gerado com sucesso: dados_brutos.json")
 
         print(type(extracted_data))
 
         return extracted_data
 
-    def _parse_lab_field(self, match: Optional[re.Match]) -> str:
+    @staticmethod
+    def _parse_lab_field(match: Optional[re.Match]) -> str:
         if not match:
             return "Não se aplica"
 
@@ -61,7 +72,7 @@ class DocsExtractor:
             return "Sim"
         elif nao == 'X':
             return "Não"
-        elif match.lastindex >= 3 and match.group(3):
+        elif match.lastindex is not None and match.lastindex >= 3 and match.group(3):
             valor = f"{match.group(3).strip()}h"
             if valor.endswith("h"):
                 return "Sim" if valor != "0h" else "Não"
